b68f5192a42a3f8ee50f2868a7bc0420baa9e6a2
Joel DeSante <joeldesante@gmail.com>
2020-11-26 22:55:32 -0500

    Setup

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Printfully = void 0;
+var Printfully = /** @class */ (function () {
+    function Printfully() {
+    }
+    return Printfully;
+}());
+exports.Printfully = Printfully;
